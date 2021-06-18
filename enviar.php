f8f351ef23895018ada55225f33137987023a963
lenilsonda <86117178+lenilsonda@users.noreply.github.com>
2021-06-18 11:45:03 -0300

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,516 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+	<!-- Document Meta
+    ============================================= -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<!--IE Compatibility Meta-->
+	<meta name="author" content="DMAWD - Soluções Web!"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="description" content="PJ contabilidade">
+	<link href="assets/images/favicon/icone.png" rel="icon">
+
+	<!-- Fonts
+    ============================================= -->
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i%7CMerriweather:300,300i,400,400i,700,700i,900,900i' rel='stylesheet' type='text/css'>
+
+	<!-- Stylesheets
+    ============================================= -->
+	<link href="assets/css/external.css" rel="stylesheet">
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet">	
+	<link href="assets/css/style.css" rel="stylesheet">
+
+	<!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+	<!--[if lt IE 9]>
+      <script src="assets/js/html5shiv.js"></script>
+      <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+
+	<!-- Document Title
+    ============================================= -->
+	<title>PJ - Assessoria Contábil</title>
+	
+	<script type="text/javascript">
+			function MM_popupMsg(msg) { //v1.0
+			alert(msg);
+			}
+	</script>
+	
+</head>
+
+<body class="body-scroll" onload="MM_popupMsg('Formulário enviado com sucesso! Aguarde nosso retorno.')">
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="wrapper clearfix">	<!-- Header
+        ============================================= -->
+		<header id="navbar-spy" class="header header-1 header-transparent header-fixed">
+
+			<nav id="primary-menu" class="navbar navbar-fixed-top">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					</button>
+					
+						<a class="logo" href="index.html">
+						<img class="logo-dark" src="assets/images/logo/logo-dark.png" alt="appy Logo">
+						<img class="logo-light" src="assets/images/logo/logo-light.png" alt="appy Logo">
+					</a>
+					</div>
+					<div class="collapse navbar-collapse pull-right" id="navbar-collapse-1">
+						<ul class="nav navbar-nav nav-pos-right navbar-left nav-split">
+							<li class="active"><a data-scroll="scrollTo" href="#slider">Principal</a></li>
+							<li><a data-scroll="scrollTo" href="#about">Quem Somos</a></li>
+							<li><a data-scroll="scrollTo" href="#feature2">Áreas de Atuação</a></li>
+							<li><a data-scroll="scrollTo" href="#testimonial">Depoimentos de Clientes</a></li>
+							<li><a data-scroll="scrollTo" href="#banner">Fale Conosco</a></li>
+						</ul>
+					</div>
+					<!--/.nav-collapse -->
+				</div>
+			</nav>
+
+		</header><!-- Slider #1
+=== === === === === === === === === === === === === === -->
+<section id="slider" class="section slider carousel slider slider-navs" data-slide="1" data-autoplay="true" data-nav="true" data-dots="false" data-space="0" data-loop="true" data-speed="800">
+	<div class="slide--item bg-overlay bg-overlay-dark">
+		<div class="bg-section">
+			<img src="assets/images/slider/slide-2.jpg" alt="bg">
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 text-center wow fadeInUp" data-wow-duration="1s">
+					<div class="slide--headline">Bem-vindo ao site da<br>PJ Assessoria Contábil.</div>
+				</div>
+				<div class="col-xs-12 col-sm-6 col-sm-offset-3 col-md-6 col-md-offset-3 text-center wow fadeInUp" data-wow-duration="1s">
+					<div class="slide--bio">As mais diversas soluções para sua micro, média ou grande empresa!</div>
+				</div>
+			</div>
+			<!-- .row end -->
+		</div>
+		<!-- .container end -->
+	</div>
+	
+	<div class="slide--item bg-overlay bg-overlay-dark">
+		<div class="bg-section">
+			<img src="assets/images/slider/slide-1.jpg" alt="bg">
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 text-center wow fadeInUp" data-wow-duration="1s">
+					<div class="slide--headline">Conheça nossa equipe de ESPECIALISTAS.</div>
+				</div>
+				<div class="col-xs-12 col-sm-6 col-sm-offset-3 col-md-6 col-md-offset-3 text-center wow fadeInUp" data-wow-duration="1s">
+					<div class="slide--bio">Especialistas estão por trás de tudo que fazemos. Venha nos conhecer!</div>
+				</div>
+			</div>
+			<!-- .row end -->
+		</div>
+		<!-- .container end -->
+	</div>
+	<!-- .slide-item end -->
+</section>
+<!-- #slider end -->
+
+<!-- About
+=== === === === === === === === === === === === === === -->
+<section id="about" class="section about pb-0">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-4 wow fadeInUp">
+				<img class="img-responsive center-block" src="assets/images/team/team-4.png"/>
+			</div>
+			<!-- .col-md-6 end -->
+			<div class="col-xs-12 col-sm-12 col-md-7 col-md-offset-1" style="margin-top:-50px;">
+				<div class="heading heading-1 mt-70 wow fadeInUp" data-wow-duration="1s">
+					<h2 class="heading--title" style="color:#e3b943;">Quem Somos</h2>
+					<div class="heading--divider"></div>
+				</div>
+				<div class="about-content wow fadeInUp" data-wow-duration="1s">
+					<p align="justify">Nossa equipe é formada por experientes profissionais nas áreas de Contabilidade! Buscamos sempre oferecer soluções que superem as expectativas dos nossos clientes, estamos sempre nos atualizando de acordo com a legislação e o dinamismo do mercado.</p>
+					<p align="justify">Primamos pela rapidez, precisão, tecnologia e expertise em nossos processos. Com o objetivo de tornar nossos clientes felizes e satisfeitos.</p>
+					<p align="justify">A <strong>PJ</strong>, se destaca por ter uma relação muito próxima com sócios e gestores, e todos envolvidos nos processos de tomada de decisão. Por isto, somos vistos como consultores de negócios que entregam soluções para que as empresas obtenham os melhores resultados.</p>
+				</div>
+			</div>
+			<!-- .col-md-6 end -->
+		</div>
+		<!-- .row end -->
+	</div>
+	<!-- .container end -->
+</section>
+<!-- #feature2 end -->
+<!-- Feature #2
+=== === === === === === === === === === === === === === -->
+<section id="feature2" class="section feature feature-2 feature-left bg-white mb-60">
+	<div class="container">
+		<div class="row clearfix">
+			<div class="col-xs-12 col-sm-12 col-md-6 col-md-offset-3">
+				<div class="heading heading-1 text--center wow fadeInUp" data-wow-duration="1s">
+					<h2 class="heading--title" style="color:#172829;">Áreas de atuação</h2>
+					<p class="heading--desc">Veja algumas das áreas de atuação da <strong>PJ</strong> Contabilidade.</p>
+					<div class="heading--divider mb-80 heading--center"></div>
+				</div>
+			</div>
+			<!-- .col-md-6 end -->
+		</div>
+		<!-- .row end -->
+		<div class="row">
+			<!-- Panel #1 -->
+			<div class="col-xs-12 col-sm-12 col-md-4">
+				<div class="feature-panel wow fadeInUp" data-wow-duration="1s">
+					<div class="feature--icon">
+						<i class="icon icon-noun_740324_cc"></i>
+					</div>
+					<div class="feature--content">
+						<h3>Legalização</h3>
+						<p>Constituição, alteração e encerramentos de empresas.</p>
+					</div>
+				</div>
+				<!-- .feature-panel end -->
+			</div>
+			<!-- .col-md-4 end -->
+
+			<!-- Panel #4 -->
+			<div class="col-xs-12 col-sm-12 col-md-4">
+				<div class="feature-panel wow fadeInUp" data-wow-duration="1s">
+					<div class="feature--icon">
+						<i class="icon icon-noun_741298_cc"></i>
+					</div>
+					<div class="feature--content">
+						<h3>Contábil</h3>
+						<p>Elaboração das demonstrações contábeis e financeiras para fins gerenciais e fisco.</p>
+					</div>
+				</div>
+				<!-- .feature-panel end -->
+			</div>
+			<!-- .col-md-4 end -->
+
+			<!-- Panel #3 -->
+			<div class="col-xs-12 col-sm-12 col-md-4">
+				<div class="feature-panel wow fadeInUp" data-wow-duration="1s">
+					<div class="feature--icon">
+						<i class="icon icon-noun_741284_cc"></i>
+					</div>
+					<div class="feature--content">
+						<h3>Pessoal</h3>
+						<p>Processamento de folha de pagamentos, respeitando a legislação vigente.</p>
+					</div>
+				</div>
+				<!-- .feature-panel end -->
+			</div>
+			<!-- .col-md-4 end -->
+			
+
+			<!-- Panel #5 -->
+			<div class="col-xs-12 col-sm-12 col-md-4">
+				<div class="feature-panel wow fadeInUp" data-wow-duration="1s">
+					<div class="feature--icon">
+						<i class="icon icon-noun_741291_cc"></i>
+					</div>
+					<div class="feature--content">
+						<h3>Gestão Financeira</h3>
+						<p>Consultoria financeira para pessoas jurídicas e pessoas físicas.</p>
+					</div>
+				</div>
+				<!-- .feature-panel end -->
+			</div>
+			<!-- .col-md-4 end -->
+		
+			<!-- Panel #2 -->
+			<div class="col-xs-12 col-sm-12 col-md-4">
+				<div class="feature-panel wow fadeInUp" data-wow-duration="1s">
+					<div class="feature--icon">
+						<i class="icon icon-noun_740303_cc"></i>
+					</div>
+					<div class="feature--content">
+						<h3>Fiscal</h3>
+						<p>Apuração dos impostos de acordo com o regime tributário específico, bem como todas as obrigações acessórias.</p>
+					</div>
+				</div>
+				<!-- .feature-panel end -->
+			</div>
+			<!-- .col-md-4 end -->
+
+			<!-- Panel #6 -->
+			<div class="col-xs-12 col-sm-12 col-md-4">
+				<div class="feature-panel wow fadeInUp" data-wow-duration="1s">
+					<div class="feature--icon">
+						<i class="icon icon-noun_740314_cc"></i>
+					</div>
+					<div class="feature--content">
+						<h3>Planejamento Tributário</h3>
+						<p>Escolha do melhor regime tributário de acordo com a atividade da empresa, visando a menor carga tributária.</p>
+					</div>
+				</div>
+				<!-- .feature-panel end -->
+			</div>
+			<!-- .col-md-4 end -->
+		</div>
+		<!-- .row end -->
+	</div>
+	<!-- .container end -->
+</section>
+<!-- #feature2 end -->
+
+<!-- Testimonial #2
+=== === === === === === === === === === === === === === -->
+<section id="testimonial" class="testimonial">
+	<div class="container">
+		<div class="row clearfix">
+			<div class="col-xs-12 col-sm-12 col-md-6 col-md-offset-3">
+				<div class="heading heading-1 text--center wow fadeInUp" data-wow-duration="1s">
+					<h2  style="color:#e3b943;" class="heading--title">Depoimentos de Clientes
+					<p class="heading--desc">Vejam o que nossos clientes falam de nosso trabalho.</p>
+					<div class="heading--divider mb-80 heading--center"></div>
+				</div>
+			</div>
+			<!-- .col-md-6 end -->
+		</div>
+		<!-- .row end -->
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12">
+				<div class="carousel">
+					<!-- Testimonial #1  !--data-slide="3" data-slide-res="2" data-autoplay="true" data-nav="false" data-dots="false" data-space="30" data-loop="true" data-speed="800"-- -->
+					<div class="testimonial-panel">
+						<div class="testimonial--body">
+							<p>"Escritório muito aconchegante, excelente receptividade e equipe contábil muito rápida e eficiente! Obs: tem sempre um cafezinho passado na hora!"</p>
+						</div>
+						<!-- .testimonial-body end -->
+						<div class="testimonial--meta">
+							<h4><span style="color:#e3b943;">Glauber Gomes</span></h4>
+						</div>
+						<!-- .testimonial-meta end -->
+					</div>
+					<!-- .testimonial-panel end -->
+
+					<!-- Testimonial #2 -->
+					<div class="testimonial-panel">
+						<div class="testimonial--body">
+							<p>"Ótimo serviço de contabilidade, personalizado e adequado para qualquer tipo de serviço, aqui em nossa empresa estamos satisfeitíssimo com os serviços prestados!"</p>
+						</div>
+						<!-- .testimonial-body end -->
+						<div class="testimonial--meta">
+							<h4><span style="color:#e3b943;">Madsen Temper</span></h4>
+						</div>
+						<!-- .testimonial-meta end -->
+					</div>
+					<!-- .testimonial-panel end -->
+
+					<!-- Testimonial #3 -->
+					<div class="testimonial-panel">
+						<div class="testimonial--body">
+							<p>"Competência e profissionalismo, tem os melhores atendimentos. Sou cliente e super indico."</p>
+						</div>
+						<!-- .testimonial-body end -->
+						<div class="testimonial--meta">
+							<h4><span style="color:#e3b943;">Lucas Wesley</span></h4>
+						</div>
+						<!-- .testimonial-meta end -->
+					</div>
+					<!-- Testimonial #4 -->
+					<div class="testimonial-panel">
+						<div class="testimonial--body">
+							<p>"Excelente profissional! Atende todas as minhas demandas, sempre muito atencioso!"</p>
+						</div>
+						<!-- .testimonial-body end -->
+						<div class="testimonial--meta">
+							<h4><span style="color:#e3b943;">Carlos Roberto Russo</span></h4>
+						</div>
+						<!-- .testimonial-meta end -->
+					</div>
+					<!-- Testimonial #4 -->
+					<div class="testimonial-panel">
+						<div class="testimonial--body">
+							<p>"Profissionalismo de Excelência! Muito obrigada pelo atendimento de primeira! Nota 1000!"</p>
+						</div>
+						<!-- .testimonial-body end -->
+						<div class="testimonial--meta">
+							<h4><span style="color:#e3b943;">Rosangela Silva Costa</span></h4>
+						</div>
+						<!-- .testimonial-meta end -->
+					</div>
+					<!-- .testimonial-panel end -->
+				</div>
+			</div>
+			<!-- .col-md-12 end -->
+		</div>
+		<!-- .row end -->
+	</div>
+	<!-- .container end -->
+</section> <!-- .testimonial2 end -->
+
+
+<!-- Banner 
+=== === === === === === === === === === === === === === -->
+<section id="banner" class="section cta pt-0 pb-0" style="background-color:#fff;">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-7 col-content pl-100">
+				<div class="heading heading-1 wow fadeInUp" data-wow-duration="1s">
+					<h2 class="heading--title">Fale Conosco</h2>
+					<p class="heading--desc">Entre em contato com nossa equipe e tire suas dúvidas.</p>
+					<div class="heading--divider mb-80"></div>
+				</div>
+				<div class="form-request">
+					<div class="row">
+						<?
+
+//pega as variaveis por POST
+
+
+$servico      = $_POST["servico"];
+
+$nome      = $_POST["nome"];
+
+$email     = $_POST["email"];
+
+$telefone1	   = $_POST["telefone1"];
+
+$telefone2	   = $_POST["telefone2"];
+
+$mensagem  = $_POST["mensagem"];
+
+
+
+global $email; //função para validar a variável $email no script todo
+
+
+
+$data      = date("d/m/y");                     //função para pegar a data de envio do e-mail
+
+$hora      = date("H:i");                       //para pegar a hora com a função date
+
+
+
+//aqui envia o e-mail para você
+
+mail ("pauloj95@hotmail.com",                  //email aonde o php vai enviar os dados do form
+
+      "Fale conosco - SITE PJ - Assessoria Contábil",
+
+      "Servico: $servico\nNome: $nome\nEmail: $email\nTelefone1: $telefone1\nTelefone2: $telefone2\nMensagem: $mensagem\nData: $data\nHora: $hora",
+
+      "From: $email"
+
+     );
+
+
+
+//aqui são as configurações para enviar o e-mail para o visitante
+
+$site   = "pauloj95@hotmail.com";                    //o e-mail que aparecerá na caixa postal do visitante
+
+$titulo = "RE: E-mail Enviado";                  //titulo da mensagem enviada para o visitante
+
+$msg    = "Obrigado por entrar em contato com a PJ - Assessoria Contábil\n\n\n Aguarde nosso contato.\n\n\nServico: $servico\nNome: $nome\nEmail: $email\nTelefone1: $telefone1\nTelefone2: $telefone2\nMensagem: $mensagem\nData: $data\nHora: $hora";
+
+
+
+//aqui envia o e-mail de auto-resposta para o visitante
+
+mail("$email",
+
+     "$titulo",
+
+     "$msg",
+
+     "From: $site"
+
+    );
+
+
+
+?><br><br><br><br><p align="center">FORMULÁRIO ENVIADO COM SUCESSO!!!!</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-12 col-md-5 pr-0 pl-0">
+				<div class="col-img">
+					<div class="bg-section">
+						<img src="assets/images/background/bg-2.jpg" alt="Background"/>
+					</div>
+				</div>
+			</div>
+			<!-- .col-md-6 end-->
+		</div>
+		<!-- .row end -->
+	</div>
+	<!-- .container end -->
+</section>
+<!-- #banner2 end -->
+
+<div style="position:fixed; bottom:120px; right:15px; z-index: 999999;"><a href="https://api.whatsapp.com/send?phone=552196413-1652&text=Ol%C3%A1%20PJ%20Contabilidade,%20poderia%20me%20passar%20algumas%20informa%C3%A7%C3%B5es?" target="_blank"><img src="assets/images/img-atendimento.png" /></a></div>
+
+		<!-- Footer #5
+============================================= -->
+		<footer id="footer" class="footer">
+		<!-- Widget Section
+	============================================= -->
+	<div class="footer-widget">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-6 col-md-4 footer--widget-about">
+					<div class="footer--widget-content">
+						<img class="mb-20" src="assets/images/logo/logo-light.png" alt="logo">
+						<p>Bem-vindo ao novo site da <strong>PJ</strong> Contabilidade. Um site onde mostramos um pouco de nossas áreas de atuação. Navegue e tenha todo suporte que sua empresa merece!<br><br>Equipe <strong>PJ</strong> Contabilidade<p>
+						<div class="clearfix"></div>
+						<div class="social">
+							<a class="share-instagram" href="javascript:;" style="color:#e3b943;"><i class="fa fa-instagram"></i></a>
+							<a class="share-facebook" href="javascript:;" style="color:#e3b943;"><i class="fa fa-facebook"></i></a>
+						</div>
+					</div>
+				</div><!-- .col-md-4 end -->
+				<div class="col-xs-12 col-sm-6 col-md-4 footer--widget-recent">
+					<div class="footer--widget-title">
+						<h5>Siga-nos no Facebook</h5>
+					</div>Timeline Facebook
+				</div><!-- .col-md-4 end -->
+				<div class="col-xs-12 col-sm-6 col-md-4 footer--widget-contact">
+					<div class="footer--widget-title">
+						<h5>Fale Conosco</h5>
+					</div>
+					<div class="footer--widget-content">
+						<ul class="list-unstyled mb-0">
+							<li><i class="fa fa-map-marker" style="color:#e3b943;"></i> Rua Rua Francisco Antônio Russo, 30 - Japeri - RJ</li>
+							<li><i class="fa fa-phone" style="color:#e3b943;"></i> Ligue Já: (21) 96413-1652</li>
+							<li><i class="fa fa-whatsapp" style="color:#e3b943;"></i> Whatsapp: (21) 96413-1652</li>
+							<li><i class="fa fa-envelope-o" style="color:#e3b943;"></i> pauloj95@hotmail.com</li>
+						</ul>
+					</div>
+				</div><!-- .col-md-4 end -->
+			</div>
+		</div><!-- .container end -->
+	</div><!-- .footer-widget end -->
+			<!-- Copyrights
+	============================================= -->
+		<div class="footer--copyright">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-12 text--center">
+						<span>&copy; Copyrights <script type="text/javascript">	var d=new Date(); document.write(d.getFullYear());</script> PJ - Assessoria Contábil.</span>
+					</div>
+				</div>
+			</div>
+			<!-- .container end -->
+			</div>
+		</footer>
+		</div>
+		<!-- #wrapper end -->
+
+		<!-- Footer Scripts
+============================================= -->
+
+		
+
+		<script src="assets/js/jquery-2.2.4.min.js"></script>
+		<script src="assets/js/plugins.js"></script>
+		<script src="assets/js/functions.js"></script>
+</body>
+
+</html>
